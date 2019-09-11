4a98e1c99f2116f9c705c26888612b1d48570cfa
Mitesh Kumar Singh <46750443+miteshksingh@users.noreply.github.com>
2019-09-11 01:54:41 -0400

    Create LRUCache-O1-146.cpp

---
+++ LRUCache-O1-146.cpp
@@ -0,0 +1,50 @@
+class LRUCache {
+private:
+    int capacity;
+    list<int> lru;
+    map<int, list<int>::iterator> mp;
+    map<int, int> cache;
+    
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+    }
+    
+    int get(int key) {
+        if(cache.count(key) == 0) {
+            return -1;
+        }
+        updateLRU(key);
+        return cache[key];
+    }
+    
+    void put(int key, int value) {
+        if(cache.size() == capacity && cache.count(key) == 0){
+            evict();
+        }
+        updateLRU(key);
+        cache[key] = value;
+    }
+    
+    
+    void updateLRU(int key) {
+        if (mp.count(key) != 0) {
+            lru.erase(mp[key]);
+        }
+        lru.push_front(key);
+        mp[key] = lru.begin();
+    }
+    
+    void evict() {
+        cache.erase(lru.back());
+        mp.erase(lru.back());
+        lru.pop_back();
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */

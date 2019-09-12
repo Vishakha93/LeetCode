15d89c72a768fed45a345847abada574ee8a6703
Mitesh Kumar Singh <46750443+miteshksingh@users.noreply.github.com>
2019-09-11 23:16:46 -0400

    Create MedianOf2SortedArrays-4.cpp

---
+++ MedianOf2SortedArrays-4.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        
+        int x = nums1.size();
+        int y = nums2.size();
+        
+        int left = 0, right = x;
+        
+        while(left <= right) {
+            
+            int partitionX = (left+right)/2;
+            int partitionY = (x+y+1)/2 - partitionX;
+            
+            int maxLeftX = (partitionX == 0)? INT_MIN: nums1[partitionX-1];
+            int minRightX = (partitionX == x)? INT_MAX: nums1[partitionX];
+            
+            int maxLeftY = (partitionY == 0)? INT_MIN: nums2[partitionY-1];
+            int minRightY = (partitionY == y)? INT_MAX: nums2[partitionY];
+            
+            
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                
+                if ((x + y) % 2 == 0) {
+                    return (double)(max(maxLeftX, maxLeftY) + min(minRightX, minRightY))/2.0;
+                }
+                else {
+                    return (double) max(maxLeftX, maxLeftY);
+                }
+            }
+            else if (maxLeftX > minRightY) {
+                right = partitionX - 1;
+            }
+            else {
+                left = partitionX + 1;
+            }
+        }
+        
+        return 0.0;
+    }
+};
